637d4f4109d990c86323d522033e1cbefc01d27d
hazy1k <160259482+hazy1k@users.noreply.github.com>
2024-08-24 11:14:59 +0800

    code

--- 2.参考工程文件/10.SysTick-系统定时器/User/SysTick/bsp_SysTick.h
+++ 2.参考工程文件/10.SysTick-系统定时器/User/SysTick/bsp_SysTick.h
@@ -5,7 +5,7 @@
 
 void SysTick_Init(void);
 void Delay_us(__IO u32 nTime);
-#define Delay_ms(x) Delay_us(100*x)	 //µ¥Î»ms
+#define Delay_ms(x) Delay_us(100*x)	
 
 void SysTick_Delay_Us( __IO uint32_t us);
 void SysTick_Delay_Ms( __IO uint32_t ms);
