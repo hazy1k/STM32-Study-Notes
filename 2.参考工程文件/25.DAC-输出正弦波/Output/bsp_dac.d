fc17f8dd4611bed7b8dc8464ab348fb10c5d06c7
hazy1k <160259482+hazy1k@users.noreply.github.com>
2024-08-29 10:59:22 +0800

    code

---
+++ 2.参考工程文件/25.DAC-输出正弦波/Output/bsp_dac.d
@@ -0,0 +1,13 @@
+..\..\output\bsp_dac.o: ..\..\User\dac\bsp_dac.c
+..\..\output\bsp_dac.o: ..\..\User\./dac/bsp_dac.h
+..\..\output\bsp_dac.o: ..\..\Libraries\CMSIS\stm32f10x.h
+..\..\output\bsp_dac.o: ..\..\Libraries\CMSIS\core_cm3.h
+..\..\output\bsp_dac.o: E:\dianzi\MDK\ARM\AC5\Bin\..\include\stdint.h
+..\..\output\bsp_dac.o: ..\..\Libraries\CMSIS\system_stm32f10x.h
+..\..\output\bsp_dac.o: ..\..\User\stm32f10x_conf.h
+..\..\output\bsp_dac.o: ..\..\Libraries\FWlib\inc\stm32f10x_dac.h
+..\..\output\bsp_dac.o: ..\..\Libraries\CMSIS\stm32f10x.h
+..\..\output\bsp_dac.o: ..\..\Libraries\FWlib\inc\stm32f10x_dma.h
+..\..\output\bsp_dac.o: ..\..\Libraries\FWlib\inc\stm32f10x_gpio.h
+..\..\output\bsp_dac.o: ..\..\Libraries\FWlib\inc\stm32f10x_rcc.h
+..\..\output\bsp_dac.o: ..\..\Libraries\FWlib\inc\stm32f10x_tim.h
---
+++ 2.参考工程文件/25.DAC-输出正弦波/Output/stm32f10x_crc.d
@@ -0,0 +1,13 @@
+..\..\output\stm32f10x_crc.o: ..\..\Libraries\FWlib\src\stm32f10x_crc.c
+..\..\output\stm32f10x_crc.o: ..\..\Libraries\FWlib\inc\stm32f10x_crc.h
+..\..\output\stm32f10x_crc.o: ..\..\Libraries\CMSIS\stm32f10x.h
+..\..\output\stm32f10x_crc.o: ..\..\Libraries\CMSIS\core_cm3.h
+..\..\output\stm32f10x_crc.o: E:\dianzi\MDK\ARM\AC5\Bin\..\include\stdint.h
+..\..\output\stm32f10x_crc.o: ..\..\Libraries\CMSIS\system_stm32f10x.h
+..\..\output\stm32f10x_crc.o: ..\..\User\stm32f10x_conf.h
+..\..\output\stm32f10x_crc.o: ..\..\Libraries\FWlib\inc\stm32f10x_dac.h
+..\..\output\stm32f10x_crc.o: ..\..\Libraries\CMSIS\stm32f10x.h
+..\..\output\stm32f10x_crc.o: ..\..\Libraries\FWlib\inc\stm32f10x_dma.h
+..\..\output\stm32f10x_crc.o: ..\..\Libraries\FWlib\inc\stm32f10x_gpio.h
+..\..\output\stm32f10x_crc.o: ..\..\Libraries\FWlib\inc\stm32f10x_rcc.h
+..\..\output\stm32f10x_crc.o: ..\..\Libraries\FWlib\inc\stm32f10x_tim.h
