80d490831e74c9361b5933bce080a13239b3875b
hazy1k <160259482+hazy1k@users.noreply.github.com>
2024-08-30 10:04:48 +0800

    参考资料

---
+++ 3.开发板必备资料/模块资料/GSM/1-GSM800A配套例程/H743_Pro开发板/2.GSM_SendMessage/User/key/bsp_key.h
@@ -0,0 +1,32 @@
+#ifndef __EXTI_H
+#define	__EXTI_H
+
+#include "stm32h7xx.h"
+
+//引脚定义
+/*******************************************************/
+#define KEY1_GPIO_PORT                GPIOA
+#define KEY1_INT_GPIO_CLK_ENABLE()        __GPIOA_CLK_ENABLE();
+#define KEY1_PIN                         GPIO_PIN_0
+
+
+#define KEY2_GPIO_PORT                GPIOC
+#define KEY2_INT_GPIO_CLK_ENABLE()        __GPIOC_CLK_ENABLE();
+#define KEY2_PIN                          GPIO_PIN_13
+
+
+
+/*******************************************************/
+
+
+ /** 按键按下标置宏
+	* 按键按下为高电平，设置 KEY_ON=1， KEY_OFF=0
+	* 若按键按下为低电平，把宏设置成KEY_ON=0 ，KEY_OFF=1 即可
+	*/
+#define KEY_ON	1
+#define KEY_OFF	0
+
+void Key_GPIO_Config(void);
+uint8_t Key_Scan(GPIO_TypeDef* GPIOx,uint16_t GPIO_Pin);
+
+#endif /* __EXTI_H */
---
+++ 3.开发板必备资料/模块资料/GSM/1-GSM800A配套例程/H750_Pro开发板/V版本/1.GSM_MakePhoneCall/User/main.c
@@ -0,0 +1,195 @@
+/**
+  ******************************************************************
+  * @file    main.c
+  * @author  fire
+  * @version V1.0
+  * @date    2019-xx-xx
+  * @brief   GSM模块电话拨号测试程序
+  ******************************************************************
+  * @attention
+  *
+  * 实验平台:野火 STM32H750 开发板 
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :https://fire-stm32.taobao.com
+  *
+  ******************************************************************
+  */  
+#include "stm32h7xx.h"
+#include "main.h"
+#include "./led/bsp_led.h"
+#include "./mpu/bsp_mpu.h" 
+#include "./usart/bsp_debug_usart.h"
+#include "./gsm_gprs/bsp_gsm_usart.h"
+#include "./systick/bsp_SysTick.h"
+#include "./gsm_gprs/bsp_gsm_gprs.h"
+#include "./key/bsp_key.h"
+#include <string.h>
+
+char    testbuff[100];
+uint8_t len;
+char *  rebuff;
+
+/**
+  * @brief  主函数
+  * @param  无
+  * @retval 无
+  */
+int main(void)
+{  
+  static uint8_t timecount=0;
+	char num[20]={0};
+	/* 系统时钟初始化成480MHz */
+	SystemClock_Config();
+ 
+  SCB_EnableICache();    // 使能指令 Cache
+  SCB_EnableDCache();    // 使能数据 Cache
+    
+	SysTick_Init();
+	/* LED 端口初始化 */
+	LED_GPIO_Config();
+	
+    Key_GPIO_Config();
+    /* 配置串口1为：115200 8-N-1 */
+	DEBUG_USART_Config();
+  
+	 printf("\r\n野火BH-GSM模块拨号例程\r\n");          
+    
+    
+    //检测模块响应是否正常
+    while(gsm_init()!= GSM_TRUE)
+    {
+      printf("\r\n模块响应测试不正常！！\r\n");
+      printf("\r\n若模块响应测试一直不正常，请检查模块的连接或是否已开启电源开关\r\n");
+	  GSM_DELAY(500);
+
+    }
+    
+    printf("\r\n通过了模块响应测试，5秒后开始拨号测试...\r\n");
+    
+    /* 延时3秒再发送命令到模块 */
+    GSM_DELAY(5000);
+    
+    //拨打电话
+  
+    gsm_call("13926769552");                           //拨打112电话测试
+    rebuff = gsm_waitask(0);
+    if(strstr(rebuff,"ATD") != NULL)           //响应OK，表示模块正常接收到命令
+    {
+      printf("\r\n正在呼叫\r\n");
+      GSM_CLEAN_RX();                     //清除接收缓冲区
+      rebuff = gsm_waitask(0);            //重新等待消息
+     
+      if(strstr(rebuff,"NO CARRIER") != NULL) //响应NO CARRIER,通话结束
+       {
+         printf("\r\n通话结束\r\n");
+       }
+      else if(strstr(rebuff,"NO ANSWER") != NULL)   //响应NO ANSWER，无人接听
+      {
+        printf("\r\n你拨打的电话暂时无人接听，请稍后再拨\r\n");
+      }  
+    }
+	
+	GSM_CLEAN_RX();                     //清除接收缓冲区
+	
+    while(1)
+	{
+		if(timecount>=100)
+		{
+			if(IsRing(num)== GSM_TRUE)
+			{
+				printf("Ringing:\nFrom:%s\n请按下KEY2接听或者按下KEY1挂断\n\r",num);	
+			}
+			timecount=0;
+		}
+		if( Key_Scan(KEY2_GPIO_PORT,KEY2_PIN) == KEY_ON  )
+		{
+			GSM_HANGON();
+		}
+		if( Key_Scan(KEY1_GPIO_PORT,KEY1_PIN) == KEY_ON  )
+		{
+			GSM_HANGOFF();
+		}	
+		timecount++;
+		GSM_DELAY(10);
+	}
+	
+}
+
+
+
+/**
+  * @brief  System Clock 配置
+  *         system Clock 配置如下: 
+	*            System Clock source  = PLL (HSE)
+	*            SYSCLK(Hz)           = 480000000 (CPU Clock)
+	*            HCLK(Hz)             = 240000000 (AXI and AHBs Clock)
+	*            AHB Prescaler        = 2
+	*            D1 APB3 Prescaler    = 2 (APB3 Clock  120MHz)
+	*            D2 APB1 Prescaler    = 2 (APB1 Clock  120MHz)
+	*            D2 APB2 Prescaler    = 2 (APB2 Clock  120MHz)
+	*            D3 APB4 Prescaler    = 2 (APB4 Clock  120MHz)
+	*            HSE Frequency(Hz)    = 25000000
+	*            PLL_M                = 5
+	*            PLL_N                = 192
+	*            PLL_P                = 2
+	*            PLL_Q                = 4
+	*            PLL_R                = 2
+	*            VDD(V)               = 3.3
+	*            Flash Latency(WS)    = 4
+  * @param  None
+  * @retval None
+  */
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** 启用电源配置更新
+  */
+  HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
+  /** 配置主内稳压器输出电压
+  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
+
+  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
+  /** 初始化CPU、AHB和APB总线时钟
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 5;
+  RCC_OscInitStruct.PLL.PLLN = 192;
+  RCC_OscInitStruct.PLL.PLLP = 2;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_2;
+  RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+  RCC_OscInitStruct.PLL.PLLFRACN = 0;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+		while(1);
+  }
+  /** 初始化CPU、AHB和APB总线时钟
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
+                              |RCC_CLOCKTYPE_D3PCLK1|RCC_CLOCKTYPE_D1PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
+  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+		while(1);
+  }
+}
+/****************************END OF FILE***************************/
