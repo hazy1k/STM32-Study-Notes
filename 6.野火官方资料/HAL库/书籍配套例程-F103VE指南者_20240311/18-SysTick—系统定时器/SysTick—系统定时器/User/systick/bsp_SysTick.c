2fca4bb3276b283b8e1cf3d488942b13068b7a32
hazy1k <160259482+hazy1k@users.noreply.github.com>
2024-09-17 14:49:34 +0800

    野火官方例程

---
+++ 6.野火官方资料/HAL库/书籍配套例程-F103VE指南者_20240311/18-SysTick—系统定时器/SysTick—系统定时器/User/systick/bsp_SysTick.c
@@ -0,0 +1,67 @@
+/**
+  ******************************************************************************
+  * @file    bsp_SysTick.c
+  * @author  fire
+  * @version V1.0
+  * @date    2016-xx-xx
+  * @brief   SysTick ϵͳ�δ�ʱ��10us�жϺ�����,�ж�ʱ����������ã�
+  *          ���õ��� 1us 10us 1ms �жϡ�     
+  ******************************************************************************
+  * @attention
+  *
+  * 
+  * ��̳    :http://www.firebbs.cn
+  * �Ա�    :http://firestm32.taobao.com
+  *
+  ******************************************************************************
+  */
+  
+#include "./systick/bsp_SysTick.h"
+
+static __IO u32 TimingDelay;
+ 
+/**
+  * @brief  ����ϵͳ�δ�ʱ�� SysTick
+  * @param  ��
+  * @retval ��
+  */
+void SysTick_Init(void)
+{
+	/* SystemFrequency / 1000    1ms�ж�һ��
+	 * SystemFrequency / 100000	 10us�ж�һ��
+	 * SystemFrequency / 1000000 1us�ж�һ��
+	 */
+	if (HAL_SYSTICK_Config(SystemCoreClock / 100000))
+	{ 
+		/* Capture error */ 
+		while (1);
+	}
+}
+
+/**
+  * @brief   us��ʱ����,10usΪһ����λ
+  * @param  
+  *		@arg nTime: Delay_us( 1 ) ��ʵ�ֵ���ʱΪ 1 * 10us = 10us
+  * @retval  ��
+  */
+void Delay_us(__IO u32 nTime)
+{ 
+	TimingDelay = nTime;	
+
+	while(TimingDelay != 0);
+}
+
+/**
+  * @brief  ��ȡ���ĳ���
+  * @param  ��
+  * @retval ��
+  * @attention  �� SysTick �жϺ��� SysTick_Handler()����
+  */
+void TimingDelay_Decrement(void)
+{
+	if (TimingDelay != 0x00)
+	{ 
+		TimingDelay--;
+	}
+}
+/*********************************************END OF FILE**********************/
