b929f215d8a12509c8b98cc46534eb990538eff5
hazy1k <160259482+hazy1k@users.noreply.github.com>
2024-09-28 16:56:32 +0800

    code

--- 2.参考工程文件/28.液晶显示中英文/1.程序/1.液晶显示中英文（字库在外部FLASH）/User/flash/bsp_spi_flash.c
+++ 2.参考工程文件/28.液晶显示中英文/1.程序/1.液晶显示中英文（字库在外部FLASH）/User/flash/bsp_spi_flash.c
@@ -1,20 +1,3 @@
-  /**
-  ******************************************************************************
-  * @file    bsp_xxx.c
-  * @author  STMicroelectronics
-  * @version V1.0
-  * @date    2013-xx-xx
-  * @brief   spi flash 底层应用函数bsp 
-  ******************************************************************************
-  * @attention
-  *
-  * 实验平台:野火 F103-指南者 STM32 开发板 
-  * 论坛    :http://www.firebbs.cn
-  * 淘宝    :https://fire-stm32.taobao.com
-  *
-  ******************************************************************************
-  */
-  
 #include "./flash/bsp_spi_flash.h"
 
 static __IO uint32_t  SPITimeout = SPIT_LONG_TIMEOUT;    
