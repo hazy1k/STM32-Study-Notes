150357411ac3abcc87d9ea38ea5e6846f8e1fcd9
hazy1k <160259482+hazy1k@users.noreply.github.com>
2024-10-02 10:55:50 +0800

    野火官方代码

---
+++ 6.野火官方资料/emXGUI实战指南/1-emXGUI入门程序/2-入门程序_MCU平台/H743_挑战者_V2/freeRTOS/06-emXGUI推荐应用模版/User/board.h
@@ -0,0 +1,35 @@
+#ifndef __BOARD_H__
+#define __BOARD_H__
+
+/*
+*************************************************************************
+*                             包含的头文件
+*************************************************************************
+*/
+/* STM32 固件库头文件 */
+#include "stm32h7xx.h"
+
+/* 开发板硬件bsp头文件 */
+#include "./led/bsp_led.h" 
+//#include "./key/bsp_key.h" 
+#include "./lcd/bsp_lcd.h"
+#include "./sdram/bsp_sdram.h" 
+#include "./touch/bsp_i2c_touch.h"
+#include "./usart/bsp_usart.h"
+//#include "./flash/bsp_spi_flash.h"
+//#include "./font/fonts.h"
+#include "./flash/bsp_qspi_flash.h"
+#include "./touch/gt9xx.h"
+#include "./cm_backtrace/cm_backtrace.h"
+#include "./sd_card/bsp_sdio_sd.h"
+#include "./mpu/bsp_mpu.h" 
+
+
+/*
+*************************************************************************
+*                               函数声明
+*************************************************************************
+*/
+	
+
+#endif /* __BOARD_H__ */
