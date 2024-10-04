6ec55d568a3d1efe49580fc66e4fdc1b61600a38
hazy1k <160259482+hazy1k@users.noreply.github.com>
2024-10-04 10:27:12 +0800

    野火官方代码

---
+++ 6.野火官方资料/电机应用开发实战/开发板例程/电机教程例程_步进_有刷_无刷/improve_part/stepper—任意直线插补-逐点比较法/User/stepper/bsp_stepper_init.h
@@ -0,0 +1,104 @@
+#ifndef __BSP_STEP_MOTOR_INIT_H
+#define	__BSP_STEP_MOTOR_INIT_H
+
+#include "stm32f1xx_hal.h"
+
+/* 步进电机结构体 */
+typedef struct{
+  uint16_t pul_pin;
+  uint16_t dir_pin;
+  uint16_t en_pin;
+  uint32_t pul_channel;
+  GPIO_TypeDef *pul_port;
+  GPIO_TypeDef *dir_port;
+  GPIO_TypeDef *en_port;
+  uint16_t oc_pulse_num;
+}Stepper_TypeDef;
+
+
+
+
+/*宏定义*/
+/*******************************************************/
+#define MOTOR_PUL_TIM                        TIM8
+#define MOTOR_PUL_IRQn                       TIM8_UP_TIM13_IRQn
+#define MOTOR_PUL_IRQHandler                 TIM8_UP_TIM13_IRQHandler
+#define MOTOR_PUL_CLK_ENABLE()               __HAL_RCC_TIM8_CLK_ENABLE()
+
+/*********************X轴电机引脚定义*******************/
+//Motor 方向
+#define X_MOTOR_DIR_PIN                      GPIO_PIN_14
+#define X_MOTOR_DIR_GPIO_PORT                GPIOB
+#define X_MOTOR_DIR_GPIO_CLK_ENABLE()        __HAL_RCC_GPIOB_CLK_ENABLE()
+
+//Motor 使能
+#define X_MOTOR_EN_PIN                       GPIO_PIN_12
+#define X_MOTOR_EN_GPIO_PORT                 GPIOB
+#define X_MOTOR_EN_GPIO_CLK_ENABLE()         __HAL_RCC_GPIOB_CLK_ENABLE()
+
+//Motor 脉冲
+#define X_MOTOR_PUL_PORT                     GPIOC
+#define X_MOTOR_PUL_PIN                      GPIO_PIN_6
+#define X_MOTOR_PUL_GPIO_CLK_ENABLE()        __HAL_RCC_GPIOC_CLK_ENABLE()
+
+//定时器通道
+#define X_MOTOR_PUL_CHANNEL                  TIM_CHANNEL_1
+
+/*********************Y轴电机引脚定义*******************/
+//Motor 方向
+#define Y_MOTOR_DIR_PIN                      GPIO_PIN_15
+#define Y_MOTOR_DIR_GPIO_PORT                GPIOB        
+#define Y_MOTOR_DIR_GPIO_CLK_ENABLE()        __HAL_RCC_GPIOB_CLK_ENABLE()
+
+//Motor 使能
+#define Y_MOTOR_EN_PIN                       GPIO_PIN_13
+#define Y_MOTOR_EN_GPIO_PORT                 GPIOB                       
+#define Y_MOTOR_EN_GPIO_CLK_ENABLE()      	 __HAL_RCC_GPIOB_CLK_ENABLE()
+
+//Motor 脉冲
+#define Y_MOTOR_PUL_PORT       			         GPIOC
+#define Y_MOTOR_PUL_PIN             		     GPIO_PIN_7
+#define Y_MOTOR_PUL_GPIO_CLK_ENABLE()        __HAL_RCC_GPIOC_CLK_ENABLE()
+
+//定时器通道
+#define Y_MOTOR_PUL_CHANNEL                  TIM_CHANNEL_2
+
+
+//控制使能引脚
+/* 带参宏，可以像内联函数一样使用 */
+#define MOTOR_PUL(port, pin, x)              HAL_GPIO_WritePin(port, pin, x)
+#define MOTOR_DIR(port, pin, x)              HAL_GPIO_WritePin(port, pin, x)
+#define MOTOR_OFFLINE(port, pin, x)          HAL_GPIO_WritePin(port, pin, x)
+#define MOTOR_START(tim, channel, status)    TIM_CCxChannelCmd(tim, channel, status)
+#define MOTOR_STOP(tim, channel, status)     TIM_CCxChannelCmd(tim, channel, status)
+
+/*频率相关参数*/
+//定时器实际时钟频率为：168MHz/TIM_PRESCALER
+//其中 高级定时器的 频率为168MHz,其他定时器为84MHz
+//168/TIM_PRESCALER=28MHz
+//具体需要的频率可以自己计算
+#define TIM_PRESCALER                3
+// 定义定时器周期，输出比较模式周期设置为0xFFFF
+#define TIM_PERIOD                   0xFFFF
+
+/************************************************************/
+#define HIGH GPIO_PIN_SET	      //高电平
+#define LOW  GPIO_PIN_RESET	    //低电平
+
+#define ON  HIGH	              //开
+#define OFF LOW	                //关
+
+#define CW 	LOW		              //顺时针
+#define CCW HIGH      	        //逆时针
+
+
+//控制使能引脚
+#define MOTOR_EN(x)					HAL_GPIO_WritePin(X_MOTOR_EN_GPIO_PORT,X_MOTOR_EN_PIN,x);\
+                            HAL_GPIO_WritePin(Y_MOTOR_EN_GPIO_PORT,Y_MOTOR_EN_PIN,x)
+
+extern TIM_HandleTypeDef TIM_StepperHandle;
+extern Stepper_TypeDef step_motor[2];
+
+void stepper_Init(void);
+
+#endif /* __STEP_MOTOR_INIT_H */
---
+++ 6.野火官方资料/电机应用开发实战/开发板例程/电机教程例程_步进_有刷_无刷/improve_part/stepper—任意象限双向圆弧插补-逐点比较法/User/main.c
@@ -0,0 +1,124 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  fire
+  * @version V1.0
+  * @date    2020-xx-xx
+  * @brief   任意象限双向圆弧-逐点比较法
+  ******************************************************************************
+  * @attention
+  *
+  * 实验平台:野火 F103-指南者 STM32 开发板 
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :https://fire-stm32.taobao.com
+  *
+  ******************************************************************************
+  */ 
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "./usart/bsp_debug_usart.h"
+#include "./stepper/bsp_stepper_init.h"
+#include "./key/bsp_key.h"
+#include "./led/bsp_led.h"
+#include "./stepper/bsp_circular_interpolation.h"
+
+/**
+  * @brief  主函数
+  * @param  无
+  * @retval 无
+  */
+int main(void) 
+{
+  HAL_InitTick(0);
+	/* 初始化系统时钟为72MHz */
+	SystemClock_Config();
+	/*初始化USART 配置模式为 115200 8-N-1，中断接收*/
+	DEBUG_USART_Config();
+	printf("欢迎使用野火 电机开发板 步进电机 任意象限圆弧插补 例程\r\n");
+  /* LED初始化 */
+  LED_GPIO_Config();
+  /* 按键初始化 */
+  Key_GPIO_Config();
+  /*步进电机初始化*/
+	stepper_Init();
+	MOTOR_EN(ON);
+	while(1)
+	{
+    /* 双向圆弧 */
+    if(Key_Scan(KEY1_GPIO_PORT, KEY1_PIN) == KEY_ON)
+    {
+      Circular_InterPolation(6400 * 10, 0, 0, -6400 * 10, 1000, CW);
+      while(circular_para.motionstatus);
+      Circular_InterPolation(0, -6400 * 10, -6400 * 10, 0, 1000, CW);
+      while(circular_para.motionstatus);
+      Circular_InterPolation(-6400 * 10, 0, 0, 6400 * 10, 1000, CW);
+      while(circular_para.motionstatus);
+      Circular_InterPolation(0, 6400 * 10, 6400 * 10, 0, 1000, CW);
+    }
+    /* 逆时针圆弧 */
+    if(Key_Scan(KEY2_GPIO_PORT, KEY2_PIN) == KEY_ON)
+    {
+      Circular_InterPolation(6400 * 10, 0, 0, 6400 * 10, 1000, CCW);
+      while(circular_para.motionstatus);
+      Circular_InterPolation(0, 6400 * 10, -6400 * 10, 0, 1000, CCW);
+      while(circular_para.motionstatus);
+      Circular_InterPolation(-6400 * 10, 0, 0, -6400 * 10, 1000, CCW);
+      while(circular_para.motionstatus);
+      Circular_InterPolation(0, -6400 * 10, 6400 * 10, 0, 1000, CCW);
+    }
+	}
+} 	
+
+
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow : 
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 72000000
+  *            HCLK(Hz)                       = 72000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 2
+  *            APB2 Prescaler                 = 1
+  *            HSE Frequency(Hz)              = 8000000
+  *            HSE PREDIV1                    = 2
+  *            PLLMUL                         = 9
+  *            Flash Latency(WS)              = 0
+  * @param  None
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef clkinitstruct = {0};
+  RCC_OscInitTypeDef oscinitstruct = {0};
+  
+  /* Enable HSE Oscillator and activate PLL with HSE as source */
+  oscinitstruct.OscillatorType  = RCC_OSCILLATORTYPE_HSE;
+  oscinitstruct.HSEState        = RCC_HSE_ON;
+  oscinitstruct.HSEPredivValue  = RCC_HSE_PREDIV_DIV1;
+  oscinitstruct.PLL.PLLState    = RCC_PLL_ON;
+  oscinitstruct.PLL.PLLSource   = RCC_PLLSOURCE_HSE;
+  oscinitstruct.PLL.PLLMUL      = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&oscinitstruct)!= HAL_OK)
+  {
+    /* Initialization Error */
+    while(1); 
+  }
+
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+     clocks dividers */
+  clkinitstruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  clkinitstruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  clkinitstruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  clkinitstruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  clkinitstruct.APB1CLKDivider = RCC_HCLK_DIV2;  
+  if (HAL_RCC_ClockConfig(&clkinitstruct, FLASH_LATENCY_2)!= HAL_OK)
+  {
+    /* Initialization Error */
+    while(1); 
+  }
+}
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
