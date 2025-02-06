f6e292173a7f0c961822dfbbf7ead23b2b0c864c
hazy1k <a249007421@gmail.com>
2025-02-06 14:32:19 +0800

    code

---
+++ 2.参考工程文件/31.SDIO-SD卡测试/5.USB-外部FLASH模拟U盘/Output/mass_mal.d
@@ -0,0 +1,17 @@
+..\..\output\mass_mal.o: ..\..\Mass_Storage\src\mass_mal.c
+..\..\output\mass_mal.o: ..\..\User\./flash/fatfs_flash_spi.h
+..\..\output\mass_mal.o: ..\..\Libraries\CMSIS\stm32f10x.h
+..\..\output\mass_mal.o: ..\..\Libraries\CMSIS\core_cm3.h
+..\..\output\mass_mal.o: E:\dianzi\MDKv5\ARM\AC5\Bin\..\include\stdint.h
+..\..\output\mass_mal.o: ..\..\Libraries\CMSIS\system_stm32f10x.h
+..\..\output\mass_mal.o: ..\..\User\stm32f10x_conf.h
+..\..\output\mass_mal.o: ..\..\Libraries\FWlib\inc\stm32f10x_dma.h
+..\..\output\mass_mal.o: ..\..\Libraries\CMSIS\stm32f10x.h
+..\..\output\mass_mal.o: ..\..\Libraries\FWlib\inc\stm32f10x_gpio.h
+..\..\output\mass_mal.o: ..\..\Libraries\FWlib\inc\stm32f10x_rcc.h
+..\..\output\mass_mal.o: ..\..\Libraries\FWlib\inc\stm32f10x_sdio.h
+..\..\output\mass_mal.o: ..\..\Libraries\FWlib\inc\stm32f10x_spi.h
+..\..\output\mass_mal.o: ..\..\Libraries\FWlib\inc\stm32f10x_usart.h
+..\..\output\mass_mal.o: ..\..\Libraries\FWlib\inc\misc.h
+..\..\output\mass_mal.o: E:\dianzi\MDKv5\ARM\AC5\Bin\..\include\stdio.h
+..\..\output\mass_mal.o: ..\..\Mass_Storage\inc\mass_mal.h
---
+++ 2.参考工程文件/31.SDIO-SD卡测试/5.USB-外部FLASH模拟U盘/Output/usb_desc.d
@@ -0,0 +1,15 @@
+..\..\output\usb_desc.o: ..\..\Mass_Storage\src\usb_desc.c
+..\..\output\usb_desc.o: ..\..\Mass_Storage\inc\usb_desc.h
+..\..\output\usb_desc.o: ..\..\Libraries\CMSIS\stm32f10x.h
+..\..\output\usb_desc.o: ..\..\Libraries\CMSIS\core_cm3.h
+..\..\output\usb_desc.o: E:\dianzi\MDKv5\ARM\AC5\Bin\..\include\stdint.h
+..\..\output\usb_desc.o: ..\..\Libraries\CMSIS\system_stm32f10x.h
+..\..\output\usb_desc.o: ..\..\User\stm32f10x_conf.h
+..\..\output\usb_desc.o: ..\..\Libraries\FWlib\inc\stm32f10x_dma.h
+..\..\output\usb_desc.o: ..\..\Libraries\CMSIS\stm32f10x.h
+..\..\output\usb_desc.o: ..\..\Libraries\FWlib\inc\stm32f10x_gpio.h
+..\..\output\usb_desc.o: ..\..\Libraries\FWlib\inc\stm32f10x_rcc.h
+..\..\output\usb_desc.o: ..\..\Libraries\FWlib\inc\stm32f10x_sdio.h
+..\..\output\usb_desc.o: ..\..\Libraries\FWlib\inc\stm32f10x_spi.h
+..\..\output\usb_desc.o: ..\..\Libraries\FWlib\inc\stm32f10x_usart.h
+..\..\output\usb_desc.o: ..\..\Libraries\FWlib\inc\misc.h
