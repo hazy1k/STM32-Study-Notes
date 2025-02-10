8aa898264c1c5b665b6d9da8e4988fe27220cd07
hazy1k <a249007421@gmail.com>
2025-02-10 23:34:44 +0800

    code

--- 2.参考工程文件/30.电阻触摸屏-触摸画板/User/lcd/xpt2046.h
+++ 2.参考工程文件/30.电阻触摸屏-触摸画板/User/lcd/xpt2046.h
@@ -2,68 +2,47 @@
 #define	__XPT2046_H
 
 #include "stm32f10x.h"
+/* 中断引脚 */
+#define XPT2046_PENIRQ_GPIO_CLK  RCC_APB2Periph_GPIOE   
+#define XPT2046_PENIRQ_GPIO_PORT GPIOE
+#define XPT2046_PENIRQ_GPIO_PIN  GPIO_Pin_4
+/* 电平触发 */
+#define XPT2046_PENIRQ_ActiveLevel 0
+/* 读取触摸屏中断引脚 */
+#define XPT2046_PENIRQ_Read() GPIO_ReadInputDataBit(XPT2046_PENIRQ_GPIO_PORT, XPT2046_PENIRQ_GPIO_PIN)
+/* SPI 接口 */
+#define XPT2046_SPI_GPIO_CLK  (RCC_APB2Periph_GPIOE| RCC_APB2Periph_GPIOD)
+#define XPT2046_SPI_CS_PIN	  GPIO_Pin_13
+#define XPT2046_SPI_CS_PORT	  GPIOD
+#define	XPT2046_SPI_CLK_PIN	  GPIO_Pin_0
+#define XPT2046_SPI_CLK_PORT  GPIOE
+#define	XPT2046_SPI_MOSI_PIN  GPIO_Pin_2
+#define	XPT2046_SPI_MOSI_PORT GPIOE
+#define	XPT2046_SPI_MISO_PIN  GPIO_Pin_3
+#define	XPT2046_SPI_MISO_PORT GPIOE
+/* CS引脚控制 */
+#define XPT2046_CS_ENABLE()  GPIO_SetBits(XPT2046_SPI_CS_PORT, XPT2046_SPI_CS_PIN)    
+#define XPT2046_CS_DISABLE() GPIO_ResetBits(XPT2046_SPI_CS_PORT, XPT2046_SPI_CS_PIN)
+/* 时钟引脚控制 */  
+#define XPT2046_CLK_HIGH()   GPIO_SetBits(XPT2046_SPI_CLK_PORT, XPT2046_SPI_CLK_PIN)    
+#define XPT2046_CLK_LOW()    GPIO_ResetBits(XPT2046_SPI_CLK_PORT, XPT2046_SPI_CLK_PIN)
+/* 读写数据引脚控制 */ 
+#define XPT2046_MOSI_1()     GPIO_SetBits(XPT2046_SPI_MOSI_PORT, XPT2046_SPI_MOSI_PIN) 
+#define XPT2046_MOSI_0()     GPIO_ResetBits(XPT2046_SPI_MOSI_PORT, XPT2046_SPI_MOSI_PIN)
+#define XPT2046_MISO()       GPIO_ReadInputDataBit(XPT2046_SPI_MISO_PORT, XPT2046_SPI_MISO_PIN)
 
-
-/******************************* XPT2046 触摸屏触摸信号指示引脚定义(不使用中断) ***************************/
-#define             XPT2046_PENIRQ_GPIO_CLK                        RCC_APB2Periph_GPIOE   
-#define             XPT2046_PENIRQ_GPIO_PORT                       GPIOE
-#define             XPT2046_PENIRQ_GPIO_PIN                        GPIO_Pin_4
-
-//触屏信号有效电平
-#define             XPT2046_PENIRQ_ActiveLevel                     0
-#define             XPT2046_PENIRQ_Read()                          GPIO_ReadInputDataBit ( XPT2046_PENIRQ_GPIO_PORT, XPT2046_PENIRQ_GPIO_PIN )
-
-
-
-/******************************* XPT2046 触摸屏模拟SPI引脚定义 ***************************/
-#define             XPT2046_SPI_GPIO_CLK                         RCC_APB2Periph_GPIOE| RCC_APB2Periph_GPIOD
-
-#define             XPT2046_SPI_CS_PIN		                        GPIO_Pin_13
-#define             XPT2046_SPI_CS_PORT		                      GPIOD
-
-#define	            XPT2046_SPI_CLK_PIN	                        GPIO_Pin_0
-#define             XPT2046_SPI_CLK_PORT	                        GPIOE
-
-#define	            XPT2046_SPI_MOSI_PIN	                        GPIO_Pin_2
-#define	            XPT2046_SPI_MOSI_PORT	                      GPIOE
-
-#define	            XPT2046_SPI_MISO_PIN	                        GPIO_Pin_3
-#define	            XPT2046_SPI_MISO_PORT	                      GPIOE
-
-
-#define             XPT2046_CS_ENABLE()                          GPIO_SetBits ( XPT2046_SPI_CS_PORT, XPT2046_SPI_CS_PIN )    
-#define             XPT2046_CS_DISABLE()                         GPIO_ResetBits ( XPT2046_SPI_CS_PORT, XPT2046_SPI_CS_PIN )  
-
-#define             XPT2046_CLK_HIGH()                           GPIO_SetBits ( XPT2046_SPI_CLK_PORT, XPT2046_SPI_CLK_PIN )    
-#define             XPT2046_CLK_LOW()                            GPIO_ResetBits ( XPT2046_SPI_CLK_PORT, XPT2046_SPI_CLK_PIN ) 
-
-#define             XPT2046_MOSI_1()                             GPIO_SetBits ( XPT2046_SPI_MOSI_PORT, XPT2046_SPI_MOSI_PIN ) 
-#define             XPT2046_MOSI_0()                             GPIO_ResetBits ( XPT2046_SPI_MOSI_PORT, XPT2046_SPI_MOSI_PIN )
-
-#define             XPT2046_MISO()                               GPIO_ReadInputDataBit ( XPT2046_SPI_MISO_PORT, XPT2046_SPI_MISO_PIN )
-
-
-
-/******************************* XPT2046 触摸屏参数定义 ***************************/
-//校准触摸屏时触摸坐标的AD值相差门限 
-#define             XPT2046_THRESHOLD_CalDiff                    2               
-
-#define	            XPT2046_CHANNEL_X 	                          0x90 	          //通道Y+的选择控制字	
-#define	            XPT2046_CHANNEL_Y 	                          0xd0	          //通道X+的选择控制字
-
-//触摸参数写到FLASH里的标志
-#define							FLASH_TOUCH_PARA_FLAG_VALUE					0xA5
-
-//触摸标志写到FLASH里的地址
-#define 							FLASH_TOUCH_PARA_FLAG_ADDR						(1*1024)
-
-//触摸参数写到FLASH里的地址
-#define 							FLASH_TOUCH_PARA_ADDR									(2*1024)
-
-
+// 校准触摸屏时触摸坐标的AD值相差门限 
+#define XPT2046_THRESHOLD_CalDiff     2               
+#define	XPT2046_CHANNEL_X 	          0x90 // 通道Y+的选择控制字	
+#define	XPT2046_CHANNEL_Y 	          0xd0 // 通道X+的选择控制字
+// 触摸参数写到FLASH里的标志
+#define FLASH_TOUCH_PARA_FLAG_VALUE	  0xA5
+// 触摸标志写到FLASH里的地址
+#define FLASH_TOUCH_PARA_FLAG_ADDR	 (1*1024)
+// 触摸参数写到FLASH里的地址
+#define FLASH_TOUCH_PARA_ADDR		 (2*1024)
 /*信息输出*/
 #define XPT2046_DEBUG_ON         0
-
 #define XPT2046_INFO(fmt,arg...)           printf("<<-XPT2046-INFO->> "fmt"\n",##arg)
 #define XPT2046_ERROR(fmt,arg...)          printf("<<-XPT2046-ERROR->> "fmt"\n",##arg)
 #define XPT2046_DEBUG(fmt,arg...)          do{\
@@ -71,19 +50,16 @@
                                           printf("<<-XPT2046-DEBUG->> [%d]"fmt"\n",__LINE__, ##arg);\
                                           }while(0)
 
-/******************************* 声明 XPT2046 相关的数据类型 ***************************/
-typedef	struct          //液晶坐标结构体 
+typedef	struct // 液晶坐标结构体 
 {		
 	/*负数值表示无新数据*/
    int16_t x;			//记录最新的触摸参数值
    int16_t y; 
-	
 	/*用于记录连续触摸时(长按)的上一次触摸位置*/
-	 int16_t pre_x;		
+   int16_t pre_x;		
    int16_t pre_y;
 	
-} strType_XPT2046_Coordinate;   
-
+}strType_XPT2046_Coordinate;   
 
 typedef struct         //校准因子结构体 
 {
@@ -109,35 +85,28 @@ typedef struct         //校准系数结构体（最终使用）
 
 } strType_XPT2046_TouchPara;
 
-/******触摸状态机相关******/
-typedef enum
+typedef enum // 触摸状态
 {
 	XPT2046_STATE_RELEASE  = 0,	//触摸释放
 	XPT2046_STATE_WAITING,			//触摸按下
 	XPT2046_STATE_PRESSED,			//触摸按下
 }enumTouchState	;
 
-#define TOUCH_PRESSED 				1
-#define TOUCH_NOT_PRESSED			0
-
-//触摸消抖阈值
+/* 触摸状态定义 */
+#define TOUCH_PRESSED 		1
+#define TOUCH_NOT_PRESSED	0
+// 触摸消抖阈值
 #define DURIATION_TIME				2
 
-/******************************* 声明 XPT2046 相关的外部全局变量 ***************************/
 extern volatile uint8_t               ucXPT2046_TouchFlag;
-
 extern strType_XPT2046_TouchPara      strXPT2046_TouchPara[];
 
-
-
-/******************************** XPT2046 触摸屏函数声明 **********************************/
-void XPT2046_Init( void );
-uint8_t XPT2046_Touch_Calibrate( uint8_t LCD_Mode );
-uint8_t XPT2046_Get_TouchedPoint( strType_XPT2046_Coordinate * displayPtr, strType_XPT2046_TouchPara * para );
-void XPT2046_TouchDown(strType_XPT2046_Coordinate * touch);
-void XPT2046_TouchUp(strType_XPT2046_Coordinate * touch);
-void XPT2046_TouchEvenHandler(void );
+void XPT2046_Init(void);
+uint8_t XPT2046_Touch_Calibrate(uint8_t LCD_Mode);
+uint8_t XPT2046_Get_TouchedPoint(strType_XPT2046_Coordinate* displayPtr, strType_XPT2046_TouchPara* para);
+void XPT2046_TouchDown(strType_XPT2046_Coordinate* touch);
+void XPT2046_TouchUp(strType_XPT2046_Coordinate* touch);
+void XPT2046_TouchEvenHandler(void);
 void Calibrate_or_Get_TouchParaWithFlash(uint8_t LCD_Mode,uint8_t forceCal);
 
-#endif /* __BSP_TOUCH_H */
-
+#endif /* __XPT2046_H */
